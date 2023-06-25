d0c5d0f54a272cd084f690a6f9e360c63682ca04
RobertGleason2 <91793071+RobertGleason2@users.noreply.github.com>
2023-06-25 11:55:21 -0400

    Update Server.py to Encrypt and Decrypt
    Server now encrypts and decrypts incoming and sent messages. Also added class and functions to the server.

--- Server.py
+++ Server.py
@@ -1,36 +1,77 @@
 #Robert Gleason and Jacob Sprouse
 import socket
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+from Cryptodome.Util.Padding import pad, unpad
 
-#create a socket object that will listen
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#get the local machine name
-host = socket.gethostname()
-#this is the port that the socket will listen on 
-port = 7777
-#bind the socket to the port, making a bridge
-serverSocket.bind((host,port))
-#start listening 
-serverSocket.listen()
 
-while True:
+class Socket(object):
+
+    @staticmethod
+    def Server():
+        # create a socket object that will listen
+        hostServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        return hostServerSocket
+
+    @staticmethod
+    def Host():
+        # get the local machine name
+        serverHost = socket.gethostname()
+        return serverHost
+
+    @staticmethod
+    def Port():
+        # this is the port that the socket will listen on
+        serverPort = 7777
+        return serverPort
+
+
+if __name__== "__main__":
+    serverSocket = Socket.Server()
+
+    host = Socket.Host()
+
+    port = Socket.Port()
+
+    # bind the socket to the port, making a bridge
+    serverSocket.bind((host, port))
+    # start listening
+    serverSocket.listen()
+
     print("Waiting for connection.....")
     # addr_port is a tuple that contains both the address and the port number
-    # clientSocket is the communication variable between the host and port
+    # clientScoket is the communication variable between the host and port
     clientSocket, addr_port = serverSocket.accept()
 
     print("\nGot a connection from " + str(addr_port))
+    while True:
+        recievedKey = clientSocket.recv(1024)
 
-    recievedBytes = clientSocket.recv(1024)
+        recievedCipherText = clientSocket.recv(1024)
 
-    recievedMessage = bytes.decode(recievedBytes)
+        decryptCipher = AES.new(recievedKey, AES.MODE_ECB)
+        encryptCipher = AES.new(recievedKey, AES.MODE_ECB)
 
-    print (recievedMessage)
+        decryptedBytes = unpad(decryptCipher.decrypt(recievedCipherText), AES.block_size)
 
-    #Response
-    message = "Hi"
+        recievedMessage = bytes.decode(decryptedBytes)
 
-    messageBytes = message.encode()
+        print(f"The cipher text is {recievedCipherText} and the message is {recievedMessage}" )
 
-    clientSocket.send(messageBytes)
+        # Response
+        message = input()
 
-    clientSocket.close()
+        messageBytes = message.encode()
+
+        cipherText = encryptCipher.encrypt(pad(messageBytes,AES.block_size))
+
+        print(cipherText)
+
+        clientSocket.send(cipherText)
+
+        if recievedMessage == "Bye" or recievedMessage == "bye":
+            clientSocket.close()
+            print("Back to listening...")
+            serverSocket.listen()
+            clientSocket, addr_port = serverSocket.accept()
+
