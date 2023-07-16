8985aeccb6e2604b404be6ab578c5dcab2e412df
RobertGleason2 <91793071+RobertGleason2@users.noreply.github.com>
2023-07-16 13:39:11 -0400

    Update Server.py to encrypt with CBC
    Server can now encrypt and decrypt in CBC mode, however there are issues with sending and receiving data across the sockets

--- Server.py
+++ Server.py
@@ -1,8 +1,10 @@
-#Robert Gleason and Jacob Sprouse
+# Robert Gleason and Jacob Sprouse
+# version 4
 import socket
 from Cryptodome.Cipher import AES
 from Cryptodome.Random import get_random_bytes
 from Cryptodome.Util.Padding import pad, unpad
+import time
 
 
 class Socket(object):
@@ -26,7 +28,45 @@ class Socket(object):
         return serverPort
 
 
-if __name__== "__main__":
+class Cipher(object):
+    @staticmethod
+    def cipher_key(key):
+        keyval = key // 8
+        cipher_key = get_random_bytes(keyval)
+        return cipher_key
+
+    @staticmethod
+    def encryption_ecb(recieved_key, message):
+        message_bytes = message.encode()
+        encryptCipher = AES.new(recieved_key, AES.MODE_ECB)
+        cipherText = encryptCipher.encrypt(pad(message_bytes, AES.block_size))
+        return cipherText
+
+    @staticmethod
+    def decryption_ecb(recieved_key, recieved_cipher_text):
+        decrypt_cipher = AES.new(recieved_key, AES.MODE_ECB)
+        decrypted_bytes = unpad(decrypt_cipher.decrypt(recieved_cipher_text), AES.block_size)
+        recieved_message = bytes.decode(decrypted_bytes)
+
+        return recieved_message
+
+    @staticmethod
+    def encryption_cbc(received_key, message, iv):
+        message_bytes = message.encode()
+        encryption_cipher = AES.new(received_key, AES.MODE_CBC, iv)
+        cipherText = encryption_cipher.encrypt(pad(message_bytes, AES.block_size))
+        return cipherText
+
+    @staticmethod
+    def decryption_cbc(received_key, received_cipher_text, iv):
+        decrypt_cipher = AES.new(received_key, AES.MODE_CBC, iv)
+        decrypted_bytes = unpad(decrypt_cipher.decrypt(received_cipher_text), AES.block_size)
+        recieved_message = bytes.decode(decrypted_bytes)
+
+        return recieved_message
+
+
+if __name__ == "__main__":
     serverSocket = Socket.Server()
 
     host = Socket.Host()
@@ -45,33 +85,59 @@ if __name__== "__main__":
 
     print("\nGot a connection from " + str(addr_port))
     while True:
-        recievedKey = clientSocket.recv(1024)
+        received_cipher_mode = clientSocket.recv(1024)
 
-        recievedCipherText = clientSocket.recv(1024)
+        print(received_cipher_mode)
 
-        decryptCipher = AES.new(recievedKey, AES.MODE_ECB)
-        encryptCipher = AES.new(recievedKey, AES.MODE_ECB)
+        received_key = clientSocket.recv(1024)
 
-        decryptedBytes = unpad(decryptCipher.decrypt(recievedCipherText), AES.block_size)
+        received_cipher_text = clientSocket.recv(1024)
+        time.sleep(2)
+        # print(received_cipher_text)
+        match received_cipher_mode.decode():
+            case "ECB":
 
-        recievedMessage = bytes.decode(decryptedBytes)
+                received_message = Cipher.decryption_ecb(received_key, received_cipher_text)
 
-        print(f"The cipher text is {recievedCipherText} and the message is {recievedMessage}" )
+                print(f"The cipher text is {received_cipher_text} and the message is {received_message}")
 
-        # Response
-        message = input()
+                # Response
+                message = input()
 
-        messageBytes = message.encode()
+                cipher_text = Cipher.encryption_ecb(received_key, message)
 
-        cipherText = encryptCipher.encrypt(pad(messageBytes,AES.block_size))
+                print(cipher_text)
 
-        print(cipherText)
+                clientSocket.send(cipher_text)
 
-        clientSocket.send(cipherText)
+                if received_message == "Bye" or received_message == "bye":
+                    clientSocket.close()
+                    print("Back to listening...")
+                    serverSocket.listen()
+                    clientSocket, addr_port = serverSocket.accept()
 
-        if recievedMessage == "Bye" or recievedMessage == "bye":
-            clientSocket.close()
-            print("Back to listening...")
-            serverSocket.listen()
-            clientSocket, addr_port = serverSocket.accept()
+            case "CBC":
+                """CBC currently doesn't work properly as the socket receiving the data is being overloaded"""
+                print(f"Ciphertext{received_cipher_text}")
+                received_iv = clientSocket.recv(1024)
+                print(received_iv)
+
+                received_message = Cipher.decryption_cbc(received_key, received_cipher_text, received_iv)
+
+                print(f"The cipher text is {received_cipher_text} and the message is {received_message}")
+
+                # Response
+                message = input()
+
+                cipher_text = Cipher.encryption_cbc(received_key, message, received_iv)
+
+                print(cipher_text)
+
+                clientSocket.send(cipher_text)
+                if received_message == "Bye" or received_message == "bye":
+                    clientSocket.close()
+                    print("Back to listening...")
+                    serverSocket.listen()
+                    clientSocket, addr_port = serverSocket.accept()
+
 
