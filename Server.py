22df61097e6899086d6a5a8ed4f3c80151f3c5c2
RobertGleason2 <91793071+RobertGleason2@users.noreply.github.com>
2023-08-09 11:56:43 -0400

    Update Server.py
    CBC mode now works as intended and added OFB mode

--- Server.py
+++ Server.py
@@ -1,70 +1,7 @@
 # Robert Gleason and Jacob Sprouse
-# version 4
-import socket
-from Cryptodome.Cipher import AES
-from Cryptodome.Random import get_random_bytes
-from Cryptodome.Util.Padding import pad, unpad
-import time
+# version 5
 
-
-class Socket(object):
-
-    @staticmethod
-    def Server():
-        # create a socket object that will listen
-        hostServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        return hostServerSocket
-
-    @staticmethod
-    def Host():
-        # get the local machine name
-        serverHost = socket.gethostname()
-        return serverHost
-
-    @staticmethod
-    def Port():
-        # this is the port that the socket will listen on
-        serverPort = 7777
-        return serverPort
-
-
-class Cipher(object):
-    @staticmethod
-    def cipher_key(key):
-        keyval = key // 8
-        cipher_key = get_random_bytes(keyval)
-        return cipher_key
-
-    @staticmethod
-    def encryption_ecb(recieved_key, message):
-        message_bytes = message.encode()
-        encryptCipher = AES.new(recieved_key, AES.MODE_ECB)
-        cipherText = encryptCipher.encrypt(pad(message_bytes, AES.block_size))
-        return cipherText
-
-    @staticmethod
-    def decryption_ecb(recieved_key, recieved_cipher_text):
-        decrypt_cipher = AES.new(recieved_key, AES.MODE_ECB)
-        decrypted_bytes = unpad(decrypt_cipher.decrypt(recieved_cipher_text), AES.block_size)
-        recieved_message = bytes.decode(decrypted_bytes)
-
-        return recieved_message
-
-    @staticmethod
-    def encryption_cbc(received_key, message, iv):
-        message_bytes = message.encode()
-        encryption_cipher = AES.new(received_key, AES.MODE_CBC, iv)
-        cipherText = encryption_cipher.encrypt(pad(message_bytes, AES.block_size))
-        return cipherText
-
-    @staticmethod
-    def decryption_cbc(received_key, received_cipher_text, iv):
-        decrypt_cipher = AES.new(received_key, AES.MODE_CBC, iv)
-        decrypted_bytes = unpad(decrypt_cipher.decrypt(received_cipher_text), AES.block_size)
-        recieved_message = bytes.decode(decrypted_bytes)
-
-        return recieved_message
-
+from Classes import Socket, Cipher
 
 if __name__ == "__main__":
     serverSocket = Socket.Server()
@@ -90,9 +27,8 @@ if __name__ == "__main__":
         print(received_cipher_mode)
 
         received_key = clientSocket.recv(1024)
-
         received_cipher_text = clientSocket.recv(1024)
-        time.sleep(2)
+
         # print(received_cipher_text)
         match received_cipher_mode.decode():
             case "ECB":
@@ -117,9 +53,10 @@ if __name__ == "__main__":
                     clientSocket, addr_port = serverSocket.accept()
 
             case "CBC":
-                """CBC currently doesn't work properly as the socket receiving the data is being overloaded"""
-                print(f"Ciphertext{received_cipher_text}")
+
+                print(f"Ciphertext {received_cipher_text}")
                 received_iv = clientSocket.recv(1024)
+
                 print(received_iv)
 
                 received_message = Cipher.decryption_cbc(received_key, received_cipher_text, received_iv)
@@ -130,6 +67,30 @@ if __name__ == "__main__":
                 message = input()
 
                 cipher_text = Cipher.encryption_cbc(received_key, message, received_iv)
+
+                print(cipher_text)
+
+                clientSocket.send(cipher_text)
+                if received_message == "Bye" or received_message == "bye":
+                    clientSocket.close()
+                    print("Back to listening...")
+                    serverSocket.listen()
+                    clientSocket, addr_port = serverSocket.accept()
+            case "OFB":
+
+                print(f"Ciphertext {received_cipher_text}")
+                received_iv = clientSocket.recv(1024)
+
+                print(received_iv)
+
+                received_message = Cipher.decryption_ofb(received_key, received_cipher_text, received_iv)
+
+                print(f"The cipher text is {received_cipher_text} and the message is {received_message}")
+
+                # Response
+                message = input()
+
+                cipher_text = Cipher.encryption_ofb(received_key, message, received_iv)
 
                 print(cipher_text)
 
