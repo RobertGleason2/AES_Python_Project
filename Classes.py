7c56f7168c602657e1bad41418cb89bc19de3331
RobertGleason2 <91793071+RobertGleason2@users.noreply.github.com>
2023-08-20 00:36:33 -0400

    Update Classes.py
    Updated the classes being used to include methods for RSA encryption. Updated the encryption methods for AES

--- Classes.py
+++ Classes.py
@@ -1,7 +1,9 @@
 import socket
 from Cryptodome.Cipher import AES
+from Crypto.PublicKey import RSA
 from Cryptodome.Random import get_random_bytes
 from Cryptodome.Util.Padding import pad, unpad
+from Crypto.Cipher import PKCS1_OAEP
 
 
 class Socket(object):
@@ -33,24 +35,50 @@ class Cipher(object):
         return cipher_key
 
     @staticmethod
-    def encryption_ecb(recieved_key, message):
+    def encryption_ecb(received_key, message, user):
+        try:
+            if user.lower() == 'client':
+                rsa_key_data = RSA.import_key(open("server_private.pem").read()).export_key()
+                rsa_key = RSA.import_key(rsa_key_data)
+            elif user.lower() == "server":
+                rsa_key_data = RSA.import_key(open("client_private.pem").read()).export_key()
+                rsa_key = RSA.import_key(rsa_key_data)
+            else:
+                raise ValueError("User Error")
+        except ValueError as ve:
+            print("Error", ve)
+
+        cipher_key = Cipher.decrypt_rsa(rsa_key, received_key)
         message_bytes = message.encode()
-        encryptCipher = AES.new(recieved_key, AES.MODE_ECB)
+        encryptCipher = AES.new(cipher_key, AES.MODE_ECB)
         cipherText = encryptCipher.encrypt(pad(message_bytes, AES.block_size))
         return cipherText
 
     @staticmethod
-    def decryption_ecb(recieved_key, recieved_cipher_text):
-        decrypt_cipher = AES.new(recieved_key, AES.MODE_ECB)
-        decrypted_bytes = unpad(decrypt_cipher.decrypt(recieved_cipher_text), AES.block_size)
-        recieved_message = bytes.decode(decrypted_bytes)
+    def decryption_ecb(received_key, received_cipher_text):
+        decrypt_cipher = AES.new(received_key, AES.MODE_ECB)
+        decrypted_bytes = unpad(decrypt_cipher.decrypt(received_cipher_text), AES.block_size)
+        received_message = bytes.decode(decrypted_bytes)
 
-        return recieved_message
+        return received_message
 
     @staticmethod
-    def encryption_cbc(received_key, message, iv):
+    def encryption_cbc(received_key, message, iv, user):
+        try:
+            if user.lower() == 'client':
+                rsa_key_data = RSA.import_key(open("server_private.pem").read()).export_key()
+                rsa_key = RSA.import_key(rsa_key_data)
+            elif user.lower() == "server":
+                rsa_key_data = RSA.import_key(open("client_private.pem").read()).export_key()
+                rsa_key = RSA.import_key(rsa_key_data)
+            else:
+                raise ValueError("User Error")
+        except ValueError as ve:
+            print("Error", ve)
+
+        cipher_key = Cipher.decrypt_rsa(rsa_key, received_key)
         message_bytes = message.encode()
-        encryption_cipher = AES.new(received_key, AES.MODE_CBC, iv)
+        encryption_cipher = AES.new(cipher_key, AES.MODE_CBC, iv)
         cipherText = encryption_cipher.encrypt(pad(message_bytes, AES.block_size))
         return cipherText
 
@@ -58,19 +86,66 @@ class Cipher(object):
     def decryption_cbc(received_key, received_cipher_text, iv):
         decrypt_cipher = AES.new(received_key, AES.MODE_CBC, iv)
         decrypted_bytes = unpad(decrypt_cipher.decrypt(received_cipher_text), AES.block_size)
-        recieved_message = bytes.decode(decrypted_bytes)
+        received_message = bytes.decode(decrypted_bytes)
 
-        return recieved_message
+        return received_message
+
     @staticmethod
-    def encryption_ofb(received_key, message, iv):
+    def encryption_ofb(received_key, message, iv, user):
+        try:
+            if user.lower() == 'client':
+                rsa_key_data = RSA.import_key(open("server_private.pem").read()).export_key()
+                rsa_key = RSA.import_key(rsa_key_data)
+            elif user.lower() == "server":
+                rsa_key_data = RSA.import_key(open("client_private.pem").read()).export_key()
+                rsa_key = RSA.import_key(rsa_key_data)
+            else:
+                raise ValueError("User Error")
+        except ValueError as ve:
+            print("Error", ve)
+
+        cipher_key = Cipher.decrypt_rsa(rsa_key, received_key)
         message_bytes = message.encode()
-        encryption_cipher = AES.new(received_key, AES.MODE_OFB, iv)
+        encryption_cipher = AES.new(cipher_key, AES.MODE_OFB, iv)
         cipher_text = encryption_cipher.encrypt(message_bytes)
         return cipher_text
+
     @staticmethod
     def decryption_ofb(received_key, received_cipher_text, iv):
         decrypt_cipher = AES.new(received_key, AES.MODE_OFB, iv)
-        decrpted_bytes = decrypt_cipher.decrypt(received_cipher_text)
-        received_message = bytes.decode(decrpted_bytes)
+        decrypted_bytes = decrypt_cipher.decrypt(received_cipher_text)
+        received_message = bytes.decode(decrypted_bytes)
         return received_message
 
+    @staticmethod
+    def generate_rsa_key():
+        key = RSA.generate(2048)
+        return key
+
+    @staticmethod
+    def generate_privk(key, filename):
+        private_key = key.export_key()
+        file_out = open(filename, 'wb')
+        file_out.write(private_key)
+        file_out.close()
+        return private_key
+
+    @staticmethod
+    def generate_pk(key, filename):
+        public_key = key.publickey().export_key()
+        file_out = open(filename, 'wb')
+        file_out.write(public_key)
+        file_out.close()
+        return public_key
+
+    @staticmethod
+    def encrypt_rsa(private_key, aes_key):
+        rsa_cipher = PKCS1_OAEP.new(private_key)
+        encrypt_rsa_key = rsa_cipher.encrypt(aes_key)
+        return encrypt_rsa_key
+
+    @staticmethod
+    def decrypt_rsa(public_key, encrypted_rsa_key):
+        rsa_cipher = PKCS1_OAEP.new(public_key)
+        decrypt_rsa_key = rsa_cipher.decrypt(encrypted_rsa_key)
+        return decrypt_rsa_key
