94a1650d057cf1db5a2f3aa8d923ad37cdc6b5b2
RobertGleason2 <91793071+RobertGleason2@users.noreply.github.com>
2023-08-20 00:33:45 -0400

    Update Server.py
    Server now sends and receives messages encoded in AES symmetric encryption and RSA public key encryption. After receiving a response from server side, Client and Server no longer continue to communicate but connection stays up

--- Server.py
+++ Server.py
@@ -1,13 +1,15 @@
 # Robert Gleason and Jacob Sprouse
-# version 5
+# version 6
+import time
 
 from Classes import Socket, Cipher
+from Crypto.PublicKey import RSA
 
 if __name__ == "__main__":
     serverSocket = Socket.Server()
 
     host = Socket.Host()
-
+    name = "server"
     port = Socket.Port()
 
     # bind the socket to the port, making a bridge
@@ -20,31 +22,50 @@ if __name__ == "__main__":
     # clientScoket is the communication variable between the host and port
     clientSocket, addr_port = serverSocket.accept()
 
+    """ Creates RSA key for server"""
+    rsa_key = Cipher.generate_rsa_key()
+    rsa_private = Cipher.generate_privk(rsa_key, 'server_private.pem')
+    rsa_public = Cipher.generate_pk(rsa_key, 'server_public.pem')
+
+    """Reads the key from the generated files and exports the key"""
+    rsa_server_public_key_data = RSA.import_key(open("server_public.pem").read()).export_key()
+    rsa_server_private_key_data = RSA.import_key(open("server_private.pem").read()).export_key()
+
+    """imports the data of the server's private key from the exported data"""
+    rsa_server_private_key = RSA.import_key(rsa_server_private_key_data)
+
+    """sends the public key data to the client """
+    clientSocket.send(rsa_server_public_key_data)
+
     print("\nGot a connection from " + str(addr_port))
     while True:
+        client_public_key_data = clientSocket.recv(2048)
+        client_public_key = RSA.import_key(client_public_key_data)
+        print(f"KEy client{client_public_key}")
+
         received_cipher_mode = clientSocket.recv(1024)
-
-        print(received_cipher_mode)
-
-        received_key = clientSocket.recv(1024)
+        received_aes_key = clientSocket.recv(1024)
         received_cipher_text = clientSocket.recv(1024)
 
+        decrypted_aes_key = Cipher.decrypt_rsa(rsa_server_private_key, received_aes_key)
+        print(f"RSA {received_aes_key}")
         # print(received_cipher_text)
         match received_cipher_mode.decode():
             case "ECB":
 
-                received_message = Cipher.decryption_ecb(received_key, received_cipher_text)
+                received_message = Cipher.decryption_ecb(decrypted_aes_key, received_cipher_text)
 
                 print(f"The cipher text is {received_cipher_text} and the message is {received_message}")
 
                 # Response
-                message = input()
-
-                cipher_text = Cipher.encryption_ecb(received_key, message)
+                message = input("Input a message \n")
+                encrypted_aes_key = Cipher.encrypt_rsa(client_public_key, decrypted_aes_key)
+                cipher_text = Cipher.encryption_ecb(encrypted_aes_key, message, name)
 
                 print(cipher_text)
 
                 clientSocket.send(cipher_text)
+                clientSocket.send(encrypted_aes_key)
 
                 if received_message == "Bye" or received_message == "bye":
                     clientSocket.close()
@@ -56,21 +77,20 @@ if __name__ == "__main__":
 
                 print(f"Ciphertext {received_cipher_text}")
                 received_iv = clientSocket.recv(1024)
-
-                print(received_iv)
-
-                received_message = Cipher.decryption_cbc(received_key, received_cipher_text, received_iv)
-
+                print(f"IV: {received_iv}")
+                print(f"AES Key: {decrypted_aes_key}")
+                received_message = Cipher.decryption_cbc(decrypted_aes_key, received_cipher_text, received_iv)
                 print(f"The cipher text is {received_cipher_text} and the message is {received_message}")
 
                 # Response
-                message = input()
-
-                cipher_text = Cipher.encryption_cbc(received_key, message, received_iv)
+                message = input("Input a message my boi \n")
+                encrypted_aes_key = Cipher.encrypt_rsa(client_public_key, decrypted_aes_key)
+                cipher_text = Cipher.encryption_cbc(encrypted_aes_key, message, received_iv, name)
 
                 print(cipher_text)
 
                 clientSocket.send(cipher_text)
+                clientSocket.send(encrypted_aes_key)
                 if received_message == "Bye" or received_message == "bye":
                     clientSocket.close()
                     print("Back to listening...")
@@ -80,25 +100,23 @@ if __name__ == "__main__":
 
                 print(f"Ciphertext {received_cipher_text}")
                 received_iv = clientSocket.recv(1024)
-
                 print(received_iv)
 
-                received_message = Cipher.decryption_ofb(received_key, received_cipher_text, received_iv)
-
+                received_message = Cipher.decryption_ofb(decrypted_aes_key, received_cipher_text, received_iv)
                 print(f"The cipher text is {received_cipher_text} and the message is {received_message}")
 
                 # Response
-                message = input()
-
-                cipher_text = Cipher.encryption_ofb(received_key, message, received_iv)
+                message = input("Input a message my boi \n")
+                encrypted_aes_key = Cipher.encrypt_rsa(client_public_key, decrypted_aes_key)
+                cipher_text = Cipher.encryption_ofb(encrypted_aes_key, message, received_iv, name)
 
                 print(cipher_text)
 
                 clientSocket.send(cipher_text)
+                clientSocket.send(encrypted_aes_key)
                 if received_message == "Bye" or received_message == "bye":
                     clientSocket.close()
                     print("Back to listening...")
                     serverSocket.listen()
                     clientSocket, addr_port = serverSocket.accept()
-
 
