bb7fc241623d7ca32c9f2f86ed443ab4a17cd14d
RobertGleason2 <91793071+RobertGleason2@users.noreply.github.com>
2023-10-05 11:27:27 -0400

    Update Server.py
    Added comments and descriptions to explain and clean up code

--- Server.py
+++ Server.py
@@ -1,13 +1,11 @@
 # Robert Gleason and Jacob Sprouse
-# version 6
+# version 7
 import time
-
 from Classes import Socket, Cipher
 from Crypto.PublicKey import RSA
 
 if __name__ == "__main__":
     serverSocket = Socket.Server()
-
     host = Socket.Host()
     name = "server"
     port = Socket.Port()
@@ -41,7 +39,7 @@ if __name__ == "__main__":
     while True:
         client_public_key_data = clientSocket.recv(2048)
         client_public_key = RSA.import_key(client_public_key_data)
-        print(f"KEy client{client_public_key}")
+        # print(f"Key client{client_public_key}") Debugging purposes
 
         received_cipher_mode = clientSocket.recv(1024)
         received_aes_key = clientSocket.recv(1024)
@@ -52,21 +50,19 @@ if __name__ == "__main__":
         # print(received_cipher_text)
         match received_cipher_mode.decode():
             case "ECB":
-
+                # NOTE: ECB is not secure at all, this is just for learning purposes.
+                # Decrypts the received message and prints it to the screen
                 received_message = Cipher.decryption_ecb(decrypted_aes_key, received_cipher_text)
-
                 print(f"The cipher text is {received_cipher_text} and the message is {received_message}")
 
-                # Response
+                # Gets input from the server user, encrypts the message and key and sends them to the client
                 message = input("Input a message \n")
                 encrypted_aes_key = Cipher.encrypt_rsa(client_public_key, decrypted_aes_key)
                 cipher_text = Cipher.encryption_ecb(encrypted_aes_key, message, name)
-
                 print(cipher_text)
-
                 clientSocket.send(cipher_text)
                 clientSocket.send(encrypted_aes_key)
-
+                # If message received is Bye or bye, ends the connection
                 if received_message == "Bye" or received_message == "bye":
                     clientSocket.close()
                     print("Back to listening...")
@@ -74,46 +70,43 @@ if __name__ == "__main__":
                     clientSocket, addr_port = serverSocket.accept()
 
             case "CBC":
-
+                # Decrypts the received message and prints it to the screen
                 print(f"Ciphertext {received_cipher_text}")
                 received_iv = clientSocket.recv(1024)
-                print(f"IV: {received_iv}")
-                print(f"AES Key: {decrypted_aes_key}")
+                # print(f"IV: {received_iv}") debugging purpose
+                # print(f"AES Key: {decrypted_aes_key}") debugging purpose
                 received_message = Cipher.decryption_cbc(decrypted_aes_key, received_cipher_text, received_iv)
                 print(f"The cipher text is {received_cipher_text} and the message is {received_message}")
 
-                # Response
+                # Gets input from the server user, encrypts the message and key and sends them and IV to the client
                 message = input("Input a message my boi \n")
                 encrypted_aes_key = Cipher.encrypt_rsa(client_public_key, decrypted_aes_key)
                 cipher_text = Cipher.encryption_cbc(encrypted_aes_key, message, received_iv, name)
-
                 print(cipher_text)
-
                 clientSocket.send(cipher_text)
                 clientSocket.send(encrypted_aes_key)
+                # If message received is Bye or bye, ends the connection
                 if received_message == "Bye" or received_message == "bye":
                     clientSocket.close()
                     print("Back to listening...")
                     serverSocket.listen()
                     clientSocket, addr_port = serverSocket.accept()
             case "OFB":
-
+                # Decrypts the received message and prints it to the screen
                 print(f"Ciphertext {received_cipher_text}")
                 received_iv = clientSocket.recv(1024)
                 print(received_iv)
-
                 received_message = Cipher.decryption_ofb(decrypted_aes_key, received_cipher_text, received_iv)
                 print(f"The cipher text is {received_cipher_text} and the message is {received_message}")
 
-                # Response
+                # Gets input from the server user, encrypts the message and key and sends them and IV to the client
                 message = input("Input a message my boi \n")
                 encrypted_aes_key = Cipher.encrypt_rsa(client_public_key, decrypted_aes_key)
                 cipher_text = Cipher.encryption_ofb(encrypted_aes_key, message, received_iv, name)
-
                 print(cipher_text)
-
                 clientSocket.send(cipher_text)
                 clientSocket.send(encrypted_aes_key)
+                # If message received is Bye or bye, ends the connection
                 if received_message == "Bye" or received_message == "bye":
                     clientSocket.close()
                     print("Back to listening...")
